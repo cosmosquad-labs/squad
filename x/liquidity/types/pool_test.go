031a1408928488bfcd54f953257a16ce9364f825
Hanjun Kim <hallazzang@gmail.com>
2021-12-14 02:18:00 +0900

    test: add test for PoolOperations.Deposit
    also use `int64` instead of `sdk.Int` in testcase

--- x/liquidity/types/pool_test.go
+++ x/liquidity/types/pool_test.go
@@ -37,51 +37,51 @@ func (pool *staticPool) SwapRequests() []types.SwapRequest {
 func TestPoolOperations_IsDepleted(t *testing.T) {
 	for _, tc := range []struct {
 		name       string
-		ps         sdk.Int // pool coin supply
-		rx, ry     sdk.Int // reserve balance
+		ps         int64 // pool coin supply
+		rx, ry     int64 // reserve balance
 		isDepleted bool
 	}{
 		{
 			name:       "empty pool",
-			ps:         sdk.ZeroInt(),
-			rx:         sdk.ZeroInt(),
-			ry:         sdk.ZeroInt(),
+			ps:         0,
+			rx:         0,
+			ry:         0,
 			isDepleted: true,
 		},
 		{
 			name:       "depleted, with some coins from outside",
-			ps:         sdk.ZeroInt(),
-			rx:         sdk.NewInt(100),
-			ry:         sdk.ZeroInt(),
+			ps:         0,
+			rx:         100,
+			ry:         0,
 			isDepleted: true,
 		},
 		{
 			name:       "depleted, with some coins from outside #2",
-			ps:         sdk.ZeroInt(),
-			rx:         sdk.NewInt(100),
-			ry:         sdk.NewInt(100),
+			ps:         0,
+			rx:         100,
+			ry:         100,
 			isDepleted: true,
 		},
 		{
 			name:       "normal pool",
-			ps:         types.DefaultInitialPoolCoinSupply,
-			rx:         sdk.NewInt(1000000),
-			ry:         sdk.NewInt(1000000),
+			ps:         10000,
+			rx:         10000,
+			ry:         10000,
 			isDepleted: false,
 		},
 		{
 			name:       "not depleted, but reserve coins are gone",
-			ps:         types.DefaultInitialPoolCoinSupply,
-			rx:         sdk.ZeroInt(),
-			ry:         sdk.NewInt(1000000),
+			ps:         10000,
+			rx:         0,
+			ry:         10000,
 			isDepleted: true,
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			ops := types.NewPoolOperations(&staticPool{
-				poolCoinSupply: tc.ps,
-				rx:             tc.rx,
-				ry:             tc.ry,
+				poolCoinSupply: sdk.NewInt(tc.ps),
+				rx:             sdk.NewInt(tc.rx),
+				ry:             sdk.NewInt(tc.ry),
 			})
 			require.Equal(t, tc.isDepleted, ops.IsDepleted())
 		})
@@ -91,39 +91,124 @@ func TestPoolOperations_IsDepleted(t *testing.T) {
 func TestPoolOperations_PoolPrice(t *testing.T) {
 	for _, tc := range []struct {
 		name   string
-		ps     sdk.Int // pool coin supply
-		rx, ry sdk.Int // reserve balance
+		ps     int64   // pool coin supply
+		rx, ry int64   // reserve balance
 		p      sdk.Dec // expected pool price
 	}{
 		{
 			name: "depleted pool",
-			ps:   sdk.ZeroInt(),
-			rx:   sdk.NewInt(100),
-			ry:   sdk.NewInt(100),
+			ps:   0,
+			rx:   100,
+			ry:   100,
 			p:    sdk.ZeroDec(),
 		},
 		{
 			name: "normal pool",
-			ps:   types.DefaultInitialPoolCoinSupply,
-			rx:   sdk.NewInt(200000000),
-			ry:   sdk.NewInt(1000000),
+			ps:   10000,
+			rx:   20000,
+			ry:   100,
 			p:    sdk.NewDec(200),
 		},
 		{
 			name: "decimal rounding",
-			ps:   types.DefaultInitialPoolCoinSupply,
-			rx:   sdk.NewInt(200),
-			ry:   sdk.NewInt(300),
+			ps:   10000,
+			rx:   200,
+			ry:   300,
 			p:    sdk.MustNewDecFromStr("0.666666666666666667"),
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			ops := types.NewPoolOperations(&staticPool{
-				poolCoinSupply: tc.ps,
-				rx:             tc.rx,
-				ry:             tc.ry,
+				poolCoinSupply: sdk.NewInt(tc.ps),
+				rx:             sdk.NewInt(tc.rx),
+				ry:             sdk.NewInt(tc.ry),
 			})
 			require.True(sdk.DecEq(t, tc.p, ops.PoolPrice()))
+		})
+	}
+}
+
+func TestPoolOperations_Deposit(t *testing.T) {
+	for _, tc := range []struct {
+		name   string
+		ips    int64 // initial pool coin supply
+		ps     int64 // pool coin supply
+		rx, ry int64 // reserve balance
+		x, y   int64 // depositing coin amount
+		ax, ay int64 // expected accepted coin amount
+		pc     int64 // expected minted pool coin amount
+	}{
+		{
+			name: "creating a pool",
+			ips:  10000,
+			ps:   0,
+			rx:   0,
+			ry:   0,
+			x:    100,
+			y:    100,
+			ax:   100,
+			ay:   100,
+			pc:   10000,
+		},
+		{
+			name: "reinitialize a depleted pool",
+			ips:  10000,
+			ps:   0,
+			rx:   100,
+			ry:   50,
+			x:    100,
+			y:    100,
+			ax:   100,
+			ay:   100,
+			pc:   10000,
+		},
+		// TODO: what if a pool has positive pool coin supply
+		//       but has zero reserve balance?
+		{
+			name: "ideal deposit",
+			ps:   10000,
+			rx:   2000,
+			ry:   100,
+			x:    200,
+			y:    10,
+			ax:   200,
+			ay:   10,
+			pc:   1000,
+		},
+		{
+			name: "unbalanced deposit",
+			ps:   10000,
+			rx:   2000,
+			ry:   100,
+			x:    100,
+			y:    2000,
+			ax:   100,
+			ay:   5,
+			pc:   500,
+		},
+		{
+			name: "decimal truncation",
+			ps:   333,
+			rx:   222,
+			ry:   333,
+			x:    100,
+			y:    100,
+			ax:   67,
+			ay:   100,
+			pc:   100,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			ops := types.NewPoolOperations(&staticPool{
+				initialPoolCoinSupply: sdk.NewInt(tc.ips),
+				poolCoinSupply:        sdk.NewInt(tc.ps),
+				rx:                    sdk.NewInt(tc.rx),
+				ry:                    sdk.NewInt(tc.ry),
+			})
+			ax, ay, pc := ops.Deposit(sdk.NewInt(tc.x), sdk.NewInt(tc.y))
+			require.True(sdk.IntEq(t, sdk.NewInt(tc.ax), ax))
+			require.True(sdk.IntEq(t, sdk.NewInt(tc.ay), ay))
+			require.True(sdk.IntEq(t, sdk.NewInt(tc.pc), pc))
 		})
 	}
 }
