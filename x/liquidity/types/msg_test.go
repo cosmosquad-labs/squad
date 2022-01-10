573a72345b1cc72d142c2117e7a56a3a631f01b2
kogisin <jaybkim2@gmail.com>
2022-01-10 19:15:12 +0900

    test: adding message test cases

---
+++ x/liquidity/types/msg_test.go
@@ -0,0 +1,23 @@
+package types_test
+
+import "testing"
+
+func TestMsgCreatePool(t *testing.T) {
+	// TODO: not implemented yet
+}
+
+func TestMsgDepositBatch(t *testing.T) {
+	// TODO: not implemented yet
+}
+
+func TestMsgWithdrawBatch(t *testing.T) {
+	// TODO: not implemented yet
+}
+
+func TestMsgSwapBatch(t *testing.T) {
+	// TODO: not implemented yet
+}
+
+func TestMsgCancelSwapBatch(t *testing.T) {
+	// TODO: not implemented yet
+}
