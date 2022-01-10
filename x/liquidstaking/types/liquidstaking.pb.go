ae743549d2b675c810830bbe6808d793bf5993d8
kogisin <jaybkim2@gmail.com>
2022-01-10 17:54:15 +0900

    feat: add deposit and withdraw requests

--- x/liquidstaking/types/liquidstaking.pb.go
+++ x/liquidstaking/types/liquidstaking.pb.go
@@ -500,7 +500,7 @@ func (m *Params) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field DefaultLiquidBondDenom", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field LiquidBondDenom", wireType)
 			}
 			var stringLen uint64
 			for shift := uint(0); ; shift += 7 {
