cc7da9db12b5990043e470381aa9dc06b2003fb9
Hanjun Kim <hallazzang@gmail.com>
2022-04-04 16:03:39 +0900

    test: use various CurrentEpochDays

--- x/farming/legacy/v2/store_test.go
+++ x/farming/legacy/v2/store_test.go
@@ -20,45 +20,51 @@ func TestMigrateQueuedStaking(t *testing.T) {
 
 	encCfg := chain.MakeTestEncodingConfig()
 	storeKey := sdk.NewKVStoreKey(v1farming.ModuleName)
-	ctx := testutil.DefaultContext(storeKey, sdk.NewTransientStoreKey("transient_test"))
-	ctx = ctx.WithBlockTime(now)
-	store := ctx.KVStore(storeKey)
 
-	stakingCoinDenom := "denom1"
-	farmerAcc := sdk.AccAddress(crypto.AddressHash([]byte("farmer")))
+	for _, currentEpochDays := range []uint32{1, 2, 3} {
+		t.Run("", func(t *testing.T) {
+			ctx := testutil.DefaultContext(storeKey, sdk.NewTransientStoreKey("transient_test"))
+			ctx = ctx.WithBlockTime(now)
+			store := ctx.KVStore(storeKey)
 
-	queuedStaking := types.QueuedStaking{Amount: sdk.NewInt(1000000)}
-	bz := encCfg.Marshaler.MustMarshal(&queuedStaking)
+			stakingCoinDenom := "denom1"
+			farmerAcc := sdk.AccAddress(crypto.AddressHash([]byte("farmer")))
 
-	oldKey := v1farming.GetQueuedStakingKey(stakingCoinDenom, farmerAcc)
-	store.Set(oldKey, bz)
+			queuedStaking := types.QueuedStaking{Amount: sdk.NewInt(1000000)}
+			bz := encCfg.Marshaler.MustMarshal(&queuedStaking)
 
-	const currentEpochDays = 2
-	require.NoError(t, v2farming.MigrateStore(ctx, storeKey, currentEpochDays))
+			oldKey := v1farming.GetQueuedStakingKey(stakingCoinDenom, farmerAcc)
+			store.Set(oldKey, bz)
 
-	newKey := types.GetQueuedStakingKey(now.AddDate(0, 0, currentEpochDays), stakingCoinDenom, farmerAcc)
-	require.Equal(t, bz, store.Get(newKey))
-	require.False(t, store.Has(oldKey))
+			require.NoError(t, v2farming.MigrateStore(ctx, storeKey, currentEpochDays))
+
+			newKey := types.GetQueuedStakingKey(now.AddDate(0, 0, int(currentEpochDays)), stakingCoinDenom, farmerAcc)
+			require.Equal(t, bz, store.Get(newKey))
+			require.False(t, store.Has(oldKey))
+		})
+	}
 }
 
 func TestMigrateQueuedStakingIndex(t *testing.T) {
 	now := time.Date(2022, 1, 1, 9, 0, 0, 0, time.UTC) // "2022-01-01T09:00:00Z"
 
 	storeKey := sdk.NewKVStoreKey(v1farming.ModuleName)
-	ctx := testutil.DefaultContext(storeKey, sdk.NewTransientStoreKey("transient_test"))
-	ctx = ctx.WithBlockTime(now)
-	store := ctx.KVStore(storeKey)
 
-	stakingCoinDenom := "denom1"
-	farmerAcc := sdk.AccAddress(crypto.AddressHash([]byte("farmer")))
+	for _, currentEpochDays := range []uint32{1, 2, 3} {
+		ctx := testutil.DefaultContext(storeKey, sdk.NewTransientStoreKey("transient_test"))
+		ctx = ctx.WithBlockTime(now)
+		store := ctx.KVStore(storeKey)
 
-	oldKey := v1farming.GetQueuedStakingIndexKey(farmerAcc, stakingCoinDenom)
-	store.Set(oldKey, []byte{})
+		stakingCoinDenom := "denom1"
+		farmerAcc := sdk.AccAddress(crypto.AddressHash([]byte("farmer")))
 
-	const currentEpochDays = 2
-	require.NoError(t, v2farming.MigrateStore(ctx, storeKey, currentEpochDays))
+		oldKey := v1farming.GetQueuedStakingIndexKey(farmerAcc, stakingCoinDenom)
+		store.Set(oldKey, []byte{})
 
-	newKey := types.GetQueuedStakingIndexKey(farmerAcc, stakingCoinDenom, now.AddDate(0, 0, currentEpochDays))
-	require.Equal(t, []byte{}, store.Get(newKey))
-	require.False(t, store.Has(oldKey))
+		require.NoError(t, v2farming.MigrateStore(ctx, storeKey, currentEpochDays))
+
+		newKey := types.GetQueuedStakingIndexKey(farmerAcc, stakingCoinDenom, now.AddDate(0, 0, int(currentEpochDays)))
+		require.Equal(t, []byte{}, store.Get(newKey))
+		require.False(t, store.Has(oldKey))
+	}
 }
