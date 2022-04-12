42b704261dcedaeef2e65291cf1e53f5d7267086
kogisin <jaybkim2@gmail.com>
2022-04-12 09:26:09 +0900

    test: adding mint test

--- x/mint/abci_test.go
+++ x/mint/abci_test.go
@@ -1,6 +1,7 @@
 package mint_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -191,4 +192,137 @@ func (s *ModuleTestSuite) TestImportExportGenesisEmpty() {
 	genState3 := mint.ExportGenesis(s.ctx, s.app.MintKeeper)
 	s.Require().Equal(*genState, genState2)
 	s.Require().EqualValues(genState2, *genState3)
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+func (s *ModuleTestSuite) TestSimualte_ConstantInflation() {
+	blockTime := 5 * time.Second
+
+	params := s.keeper.GetParams(s.ctx)
+	params.BlockTimeThreshold = 10 * time.Second
+	params.InflationSchedules = []types.InflationSchedule{
+		{
+			StartTime: s.ctx.BlockTime(),
+			EndTime:   s.ctx.BlockTime().AddDate(1, 0, 0),
+			Amount:    sdk.NewInt(108_700000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(1, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(2, 0, 0),
+			Amount:    sdk.NewInt(216_100000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(2, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(3, 0, 0),
+			Amount:    sdk.NewInt(151_300000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(3, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(4, 0, 0),
+			Amount:    sdk.NewInt(105_900000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(4, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(5, 0, 0),
+			Amount:    sdk.NewInt(74_100000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(5, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(6, 0, 0),
+			Amount:    sdk.NewInt(51_900000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(6, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(7, 0, 0),
+			Amount:    sdk.NewInt(36_300000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(7, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(8, 0, 0),
+			Amount:    sdk.NewInt(25_400000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(8, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(9, 0, 0),
+			Amount:    sdk.NewInt(17_800000_000000),
+		},
+		{
+			StartTime: s.ctx.BlockTime().AddDate(9, 0, 0),
+			EndTime:   s.ctx.BlockTime().AddDate(10, 0, 0),
+			Amount:    sdk.NewInt(12_500000_000000),
+		},
+	}
+	s.keeper.SetParams(s.ctx, params)
+
+	feeCollector := s.app.AccountKeeper.GetModuleAccount(s.ctx, authtypes.FeeCollectorName)
+
+	advanceHeight := func() sdk.Int {
+		nextBlockHeight := s.ctx.BlockHeight() + 1
+		nextBlockTime := s.ctx.BlockTime().Add(blockTime)
+		s.ctx = s.ctx.WithBlockHeight(nextBlockHeight)
+		s.ctx = s.ctx.WithBlockTime(nextBlockTime)
+		fmt.Printf("Advance to Next Block Height: %d\n", nextBlockHeight)
+
+		beforeBalance := s.app.BankKeeper.GetBalance(s.ctx, feeCollector.GetAddress(), sdk.DefaultBondDenom)
+		mint.BeginBlocker(s.ctx, s.app.MintKeeper)
+		afterBalance := s.app.BankKeeper.GetBalance(s.ctx, feeCollector.GetAddress(), sdk.DefaultBondDenom)
+		mintedAmt := afterBalance.Sub(beforeBalance)
+		s.Require().False(mintedAmt.IsNegative())
+		fmt.Printf("Minted Amount: %s\n", mintedAmt)
+		return mintedAmt.Amount
+	}
+
+	// Start a block time
+	startTime := s.ctx.BlockTime()
+	s.ctx = s.ctx.WithBlockHeight(0).WithBlockTime(startTime)
+	fmt.Println("--- ", startTime)
+
+	// Skip first block inflation since LastBlockTime is not set
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(0))
+
+	// after 2022-01-01 00:00:00
+	// 47564687 / 5 * (365 * 24 * 60 * 60) / 300000000000000 ~= 1
+	// 47564687 ~= 300000000000000 / (365 * 24 * 60 * 60) * 5
+	fmt.Println("advanceHeight(): ", advanceHeight())
+	fmt.Println("advanceHeight(): ", advanceHeight())
+	fmt.Println("advanceHeight(): ", advanceHeight())
+	// s.Require().EqualValues(advanceHeight(), sdk.NewInt(47564687))
+	// s.Require().EqualValues(advanceHeight(), sdk.NewInt(47564687))
+	// s.Require().EqualValues(advanceHeight(), sdk.NewInt(47564687))
+	// s.Require().EqualValues(advanceHeight(), sdk.NewInt(47564687))
+
+	startTime = s.ctx.BlockTime().AddDate(1, 0, 0)
+	s.ctx = s.ctx.WithBlockHeight(100).WithBlockTime(startTime)
+	fmt.Println("--- ", startTime)
+
+	return
+
+	// applied 10sec(params.BlockTimeThreshold) block time due to block time diff is over params.BlockTimeThreshold
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(63419583))
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(31709791))
+
+	// 317097919 / 5 * (365 * 24 * 60 * 60) / 200000000000000 ~= 1
+	// 317097919 ~= 200000000000000 / (365 * 24 * 60 * 60) * 5
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(31709791))
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(31709791))
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(31709791))
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(31709791))
+
+	blockTime = 10 * time.Second
+	// 634195839 / 10 * (365 * 24 * 60 * 60) / 200000000000000 ~= 1
+	// 634195839 ~= 200000000000000 / (365 * 24 * 60 * 60) * 10
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(63419583))
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(63419583))
+
+	// over BlockTimeThreshold 10sec
+	blockTime = 20 * time.Second
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(63419583))
+	s.Require().EqualValues(advanceHeight(), sdk.NewInt(63419583))
+
+	// no inflation
+	startTime = startTime.AddDate(1, 0, 0)
+	s.ctx = s.ctx.WithBlockHeight(300).WithBlockTime(startTime)
+	fmt.Println("--- ", startTime)
+
 }
