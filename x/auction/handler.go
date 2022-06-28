a1759bcf3c45b62337f2951d4344aa6d9dd1803a
kogisin <jaybkim2@gmail.com>
2022-06-28 14:25:44 +0900

    feat: scaffold auction module

---
+++ x/auction/handler.go
@@ -0,0 +1,26 @@
+package auction
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
+	"github.com/cosmosquad-labs/squad/x/auction/keeper"
+	"github.com/cosmosquad-labs/squad/x/auction/types"
+)
+
+// NewHandler returns a new msg handler.
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	// msgServer := keeper.NewMsgServerImpl(k)
+
+	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
+
+		switch msg := msg.(type) {
+		// TODO: not implemented yet
+		default:
+			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg))
+		}
+	}
+}
---
+++ x/auction/spec/02_state.md
@@ -0,0 +1,5 @@
+<!-- order: 2 -->
+
+# State
+
+The `liquidfarming` module keeps track of the auction and bid states.
