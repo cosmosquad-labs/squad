faf1782131787447e1b1d3189f55e96b93eda966
kogisin <jaybkim2@gmail.com>
2022-07-04 17:56:17 +0900

    feat: adding handlers for both msgs and proposal

--- x/liquidfarming/handler.go
+++ x/liquidfarming/handler.go
@@ -5,6 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	"github.com/cosmosquad-labs/squad/x/liquidfarming/keeper"
 	"github.com/cosmosquad-labs/squad/x/liquidfarming/types"
@@ -12,15 +13,42 @@ import (
 
 // NewHandler returns a new msg handler.
 func NewHandler(k keeper.Keeper) sdk.Handler {
-	// msgServer := keeper.NewMsgServerImpl(k)
+	msgServer := keeper.NewMsgServerImpl(k)
 
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
 		ctx = ctx.WithEventManager(sdk.NewEventManager())
 
 		switch msg := msg.(type) {
-		// TODO: not implemented yet
+		case *types.MsgDeposit:
+			res, err := msgServer.Deposit(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+
+		case *types.MsgCancel:
+			res, err := msgServer.Cancel(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+
+		case *types.MsgWithdraw:
+			res, err := msgServer.Withdraw(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+
+		case *types.MsgPlaceBid:
+			res, err := msgServer.PlaceBid(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
+
 		default:
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg))
+		}
+	}
+}
+
+func NewLiquidFarmProposalHandler(k keeper.Keeper) govtypes.Handler {
+	return func(ctx sdk.Context, content govtypes.Content) error {
+		switch c := content.(type) {
+		case *types.LiquidFarmProposal:
+			return keeper.CreateLiquidFarmProposal(ctx, k, c)
+
+		default:
+			return sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unrecognized liquid farm proposal content type: %T", c)
 		}
 	}
 }
---
+++ x/liquidfarming/keeper/proposal_handler.go
@@ -0,0 +1,13 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/cosmosquad-labs/squad/x/liquidfarming/types"
+)
+
+// CreateLiquidFarmProposal is a handler for executing a liquid farm creation proposal.
+func CreateLiquidFarmProposal(ctx sdk.Context, k Keeper, proposal *types.LiquidFarmProposal) error {
+
+	return nil
+}
